a7b4da23311817e39810533f8b1fefdca3dc8545
Lucas Tavernier <lucas.tavernierpro@outlook.fr>
2022-02-15 16:39:12 +0100

    doubleAuth page and back

--- email.js
+++ email.js
@@ -1,28 +1,32 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-const email = process.env.EMAIL
-const password = process.env.PASSWORD
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
 let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    auth: {
-        user: email,
-        pass: password
-    }
-})
-
-message = {
-    from: email,
-    to: "lmn.mspr@gmail.com",
-    subject: "test",
-    html: "<h1>Bonjour,</h1><p>Veuillez valider votre adresse mail pour vous connecter.</p><p>Pour cela, cliquez sur le bouton ci-dessous:</p><a href='http://localhost:8085/confirm'>Valider mon adresse mail</a>"
-}
-
-transporter.sendMail(message, function(err, info) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(info);
-    }
+	host: "smtp.gmail.com",
+	port: 465,
+	auth: {
+		user: email,
+		pass: password,
+	},
 });
+
+module.exports = function sendMail(mail, callback) {
+	message = {
+		from: email,
+		to: mail,
+		subject: "test",
+		html: "<h1>Bonjour,</h1><p>Veuillez valider votre adresse mail pour vous connecter.</p><p>Pour cela, cliquez sur le bouton ci-dessous:</p><a href='http://localhost:8085/confirm'>Valider mon adresse mail</a>",
+	};
+
+	transporter.sendMail(message, function (err, info) {
+		if (err) {
+			console.log(err);
+			return callback("problème d'envoi du mail", false);
+		} else {
+			console.log(info);
+			return callback("mail envoyé", true);
+		}
+	});
+};
--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const session = require("express-session");
 const path = require("path");
 const ActiveDirectory = require("activedirectory");
 const rateLimit = require("express-rate-limit");
+const sendMail = require("./email");
 
 var config = {
-	url: "ldap://dc.domain.com",
-	baseDN: "dc=domain,dc=com",
+	url: "ldap://mspr-admin.local",
+	baseDN: "dc=mspr-admin,dc=local",
 };
 
 //On a le droit à 5 requêtes en 1 minute, sinon on est bloqué 1 minute
@@ -22,21 +23,28 @@ var ad = new ActiveDirectory(config);
 const app = express();
 const port = 8085;
 
+app.use(
+	session({
+		secret: "secret",
+		resave: true,
+		saveUninitialized: true,
+	})
+);
+
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(limiter);
-
 
 app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname + "/views/login.html"));
 });
 
-app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname + "/views/login.html"));
+app.get("/doubleAuth", function (req, res) {
+	res.sendFile(path.join(__dirname + "/views/doubleAuth.html"));
 });
 
 app.get("/confirm", (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/confirm.html"));
+	res.sendFile(path.join(__dirname + "/views/confirm.html"));
 });
 
 app.post("/auth", function (req, res) {
@@ -46,12 +54,32 @@ app.post("/auth", function (req, res) {
 	ad.authenticate(username, password, function (err, auth) {
 		if (err) {
 			console.log("ERROR: " + JSON.stringify(err));
-			return;
+			res.status(500);
 		}
 		if (auth) {
 			console.log("Connecté !");
+			res.status(200).end();
+			res.redirect("/doubleAuth");
 		} else {
 			console.log("Connexion ratée !");
+			res.status(403).end();
+		}
+	});
+});
+
+app.post("/doubleAuth", function (req, res) {
+	let mail = req.body.email;
+
+	sendMail(mail, function (err, sended) {
+		if (err) {
+			console.log("ERROR: " + JSON.stringify(err));
+			res.status(500);
+		}
+		console.log(sended);
+		if (sended) {
+			res.status(200).end();
+		} else {
+			res.status(400).end();
 		}
 	});
 });
@@ -61,7 +89,6 @@ app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname + "/views/404.html"));
 });
 
-
 app.listen(port, () => {
-    console.log(`Serveur sur le port ${port}`);
-})
+	console.log(`Serveur sur le port ${port}`);
+});
