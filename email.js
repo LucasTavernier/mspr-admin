912b0282e7ca078e71b69188a04b76eafe5d0822
Noémie LECHERF <noemie.lecherf@epsi.fr>
2022-02-15 14:29:46 +0100

    creation .env + modification mail

--- email.js
+++ email.js
@@ -1,18 +1,22 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const email = process.env.EMAIL
+const password = process.env.PASSWORD
 let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     auth: {
-        user: "lmn.mspr@gmail.com",
-        pass: "ouiouiouimspradmin"
+        user: email,
+        pass: password
     }
 })
 
 message = {
-    from: "lmn.mspr@gmail.com",
+    from: email,
     to: "lmn.mspr@gmail.com",
     subject: "test",
-    html: "<h1>Hello SMTP Email test</h1>"
+    html: "<h1>Bonjour,</h1><p>Veuillez valider votre adresse mail pour vous connecter.</p><p>Pour cela, cliquez sur le bouton ci-dessous:</p><a href='http://localhost:8085/confirm'>Valider mon adresse mail</a>"
 }
 
 transporter.sendMail(message, function(err, info) {
