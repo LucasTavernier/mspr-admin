1e8781777fec26e82e21894d975fe0d8be3dd81c
Lucas <lucas.tavernierpro@gmail.com>
2022-03-20 15:38:49 +0100

    cleaned project

--- email.js
+++ email.js
@@ -5,33 +5,34 @@ var numUnique = uuid.v1();
 const email = process.env.EMAIL;
 const password = process.env.PASSWORD;
 let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    auth: {
-        user: email,
-        pass: password,
-    },
+	host: "smtp.gmail.com",
+	port: 465,
+	auth: {
+		user: email,
+		pass: password,
+	},
 });
 
 module.exports = function sendMail(mail, session, callback) {
-    session.uuid = numUnique;
+	session.uuid = numUnique;
 
-    message = {
-        from: email,
-        to: mail,
-        subject: "Authentification",
-        html: "<h1>Bonjour,</h1><p>Veuillez valider votre adresse mail pour vous connecter.</p><p>Pour cela, cliquez sur le bouton ci-dessous:</p><a href='http://localhost:8085/confirm/" +
-            numUnique +
-            "'>Valider mon adresse mail</a>",
-    };
+	message = {
+		from: email,
+		to: mail,
+		subject: "test",
+		html:
+			"<h1>Bonjour,</h1><p>Veuillez valider votre adresse mail pour vous connecter.</p><p>Pour cela, cliquez sur le bouton ci-dessous:</p><a href='http://localhost:8085/confirm/" +
+			numUnique +
+			"'>Valider mon adresse mail</a>",
+	};
 
-    transporter.sendMail(message, function(err, info) {
-        if (err) {
-            console.log(err);
-            return callback("problème d'envoi du mail", false);
-        } else {
-            console.log(info);
-            return callback("mail envoyé", true);
-        }
-    });
+	transporter.sendMail(message, function (err, info) {
+		if (err) {
+			console.log(err);
+			return callback("problème d'envoi du mail", false);
+		} else {
+			console.log(info);
+			return callback("mail envoyé", true);
+		}
+	});
 };
