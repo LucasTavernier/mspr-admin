80b4a294d798641d9e7d76d917a36b3e1d3538b9
Lucas Tavernier <lucas.tavernierpro@outlook.fr>
2022-03-20 17:58:57 +0100

    app functional

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-const mysql = require("mysql");
+require("dotenv").config();
+const mysql = require("mysql2");
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
 const ActiveDirectory = require("activedirectory");
 const rateLimit = require("express-rate-limit");
 const sendMail = require("./email");
+const axios = require("axios").default;
+var uuid = require("uuid");
 
 var config = {
-	url: "ldap://mspr-admin.local",
+	url: "ldap://192.168.28.131/mspr-admin.local",
 	baseDN: "dc=mspr-admin,dc=local",
 };
+
+const db = mysql.createConnection({
+	host: "127.0.0.1",
+	user: process.env.USERBDD,
+	password: process.env.PASSWORDBDD,
+	database : "mspradmin"
+  });
+
+  let ip = "";
+  axios
+  .get("https://ipapi.co/json/")
+  .then(function (response) {
+	  ip = response;
+  })
+  .catch(function (error) {
+	  console.error(error);
+  });
+
 
 //On a le droit à 5 requêtes en 1 minute, sinon on est bloqué 1 minute
 const limiter = rateLimit({
@@ -39,7 +60,7 @@ app.use(limiter);
 app.get("/", function (req, res) {
 	console.log(req.session);
 	if (req.session.loggedin) {
-		res.send("CONNECTE");
+		res.sendFile(path.join(__dirname + "/views/connecte.html"));
 	} else {
 		res.sendFile(path.join(__dirname + "/views/login.html"));
 	}
@@ -58,6 +79,20 @@ app.get("/confirm/:id", (req, res) => {
 
 	if (req.session.uuid == id) {
 		req.session.loggedin = true;
+
+		db.connect(function(err) {
+			if (err) throw err;
+			db.query("SELECT * FROM users WHERE login ='" + req.session.login+"'", function (err, result, fields) {
+			  if (err) throw err;
+			  if(result.length > 0){ //si l'utilisateur existe
+				db.query (
+					'UPDATE users '+
+					'SET mail = ?',
+					[req.session.mail]
+				);
+			  }
+			});
+		  });
 	}
 
 	res.redirect("/");
@@ -71,10 +106,64 @@ app.post("/auth", function (req, res) {
 		if (err) {
 			console.log("ERROR: " + JSON.stringify(err));
 			res.status(500);
+			res.redirect("/");
 		}
 		if (auth) {
 			console.log("Connecté !");
 			res.status(200);
+			
+			let nav = req.headers['user-agent'];
+
+			req.session.login = username;
+			db.connect(function(err) {
+				if (err) throw err;
+				db.query("SELECT * FROM users WHERE login ='" + username+"'", function (err, result, fields) {
+				  if (err) throw err;
+				  if(result.length > 0){ //si l'utilisateur existe
+					textMaybeError = "";
+					if(result[0].nav != nav){
+						textMaybeError += "Attention, une connexion suspecte a été détecté à partir" + 
+						" d'un nouveau navigateur ["+nav+"], l'habituel étant ["+result[0].nav+"].";
+					}
+					if(result[0].ip != ip.data['ip']){
+						if(textMaybeError != ""){
+							textMaybeError +="<br>De plus, une nouvelle ip a été détectée ["+ip.data['ip']+"], l'habituel étant ["+result[0].ip+"]."
+						}else{
+							textMaybeError += "Attention, une connexion suspecte a été détecté à partir" + 
+						"d'une nouvelle ip ["+ip.data['ip']+"], l'habituel étant ["+result[0].ip+"].";
+						}
+					}
+
+					if(textMaybeError != ""){
+						sendMail(result[0].mail, req.session, function (err, sended) {
+							if (err) {
+								console.log("ERROR: " + JSON.stringify(err));
+								res.status(500);
+							}
+							console.log(sended);
+							if (sended) {
+								req.session.mail = mail;
+								req.session.uuid = numUnique;
+								res.status(200).end();
+							} else {
+								res.status(400).end();
+							}
+						}, 
+						"ATTENTION CONNEXION SUSPECTE",
+						textMaybeError
+						);
+					}
+
+				  }else{
+					db.query (
+						'INSERT INTO users '+
+						'SET login = ?, nav = ?, ip = ?',
+						[username,nav, ip.data["ip"]]
+					);
+				  }
+				});
+			  });
+
 			res.redirect("/doubleAuth");
 		} else {
 			console.log("Connexion ratée !");
@@ -85,6 +174,7 @@ app.post("/auth", function (req, res) {
 
 app.post("/doubleAuth", function (req, res) {
 	let mail = req.body.email;
+	var numUnique = uuid.v1();
 
 	sendMail(mail, req.session, function (err, sended) {
 		if (err) {
@@ -93,16 +183,37 @@ app.post("/doubleAuth", function (req, res) {
 		}
 		console.log(sended);
 		if (sended) {
+			req.session.mail = mail;
+			req.session.uuid = numUnique;
 			res.status(200).end();
 		} else {
 			res.status(400).end();
 		}
-	});
+	}, 
+	"DOUBLE AUTHENTIFICATION",
+	"<h1>Bonjour,</h1><p>Veuillez valider votre adresse mail pour vous connecter.</p><p>Pour cela, cliquez sur le bouton ci-dessous:</p><a href='http://localhost/confirm/" +
+			numUnique +
+			"'>Valider mon adresse mail</a>"
+	);
 });
 
 app.get("*", (req, res) => {
-	res.status(404);
-	res.sendFile(path.join(__dirname + "/views/404.html"));
+	console.log(ip.data["country_name"]);
+	if(ip.data["country_name"] != "France"){
+		session.countryBlocked = true;
+	}else{
+		if(session.countryBlocked == true){
+			session.countryBlocked = false;
+		}
+	}
+
+	if(req.session.countryBlocked == true){
+		res.status(403);
+		res.sendFile(path.join(__dirname + "/views/interdit.html"));
+	}else{
+		res.status(404);
+		res.sendFile(path.join(__dirname + "/views/404.html"));
+	}
 });
 
 app.listen(port, () => {
